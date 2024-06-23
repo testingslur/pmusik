b69b56aefcbebc4c154edf42ad5e24df2d592e4c
testingslur <136461439+testingslur@users.noreply.github.com>
2024-06-24 06:50:45 +0700

    Update userbot.py

--- Auput/core/userbot.py
+++ Auput/core/userbot.py
@@ -53,8 +53,10 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("UputtSupport")
-                await self.one.join_chat("Flukosaa")
+                await self.one.join_chat("MT_Force")
+                await self.one.join_chat("ruangprojects")
+                await self.one.join_chat("fadzkuruunii")
+                await self.one.join_chat("fadzkuruuniialmuttaqin")
             except:
                 pass
             assistants.append(1)
@@ -83,8 +85,10 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("UputtSupport")
-                await self.two.join_chat("Flukosaa")
+                await self.two.join_chat("MT_Force")
+                await self.two.join_chat("ruangprojects")
+                await self.two.join_chat("fadzkuruunii")
+                await self.two.join_chat("fadzkuruuniialmuttaqin")
             except:
                 pass
             assistants.append(2)
@@ -113,8 +117,10 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("UputtSupport")
-                await self.three.join_chat("Flukosaa")
+                await self.three.join_chat("MT_Force")
+                await self.three.join_chat("ruangprojects")
+                await self.three.join_chat("fadzkuruunii")
+                await self.three.join_chat("fadzkuruuniialmuttaqin")
             except:
                 pass
             assistants.append(3)
@@ -143,8 +149,10 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("UputtSupport")
-                await self.four.join_chat("Flukosaa")
+                await self.four.join_chat("MT_Force")
+                await self.four.join_chat("ruangprojects")
+                await self.four.join_chat("fadzkuruunii")
+                await self.four.join_chat("fadzkuruuniialmuttaqin")
             except:
                 pass
             assistants.append(4)
@@ -173,8 +181,10 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("UputtSupport")
-                await self.five.join_chat("Flukosaa")
+                await self.five.join_chat("MT_Force")
+                await self.five.join_chat("ruangprojects")
+                await self.five.join_chat("fadzkuruunii")
+                await self.five.join_chat("fadzkuruuniialmuttaqin")
             except:
                 pass
             assistants.append(5)
