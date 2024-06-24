f01dcda8425c96e3124c9313746383be1b17ba23
testingslur <136461439+testingslur@users.noreply.github.com>
2024-06-24 09:15:08 +0700

    Update alive.py

--- Auput/plugins/tools/alive.py
+++ Auput/plugins/tools/alive.py
@@ -9,7 +9,7 @@ from config import MUSIC_BOT_NAME, OWNER_USERNAME, SUPPORT_CHANNEL
 @app.on_message(filters.command("alive", ["/", ".", "!"]))
 async def kontolmasukmemek(client: Client, message: Message):
     await message.reply_video(
-        video=f"https://telegra.ph//file/bbf47a387de772dd6a555.mp4",
+        video=f"https://telegra.ph//file/9246a6b587d88e493ced0.jpg",
         caption=f"💖 Hei {message.from_user.mention}\n\n I am {MUSIC_BOT_NAME}\n\n✨ I am Fast and Powerful music player bot with some awesome features.\n\n━━━━━━━━━━━━━━━━━━❄",
         reply_markup=InlineKeyboardMarkup(
             [
